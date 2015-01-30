35a4b4bdf177b7d44e1ba3fc98e8e7c8d9da274a
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2015-01-31 00:12:10 +0100

    changed namespace paths

--- src/JeffreyVdb/Recaptcha/CheckRecaptchaV2.php
+++ src/JeffreyVdb/Recaptcha/CheckRecaptchaV2.php
@@ -1,6 +1,4 @@
-<?php
-
-namespace Greggilbert\Recaptcha;
+<?php namespace JeffreyVdb\Recaptcha;
 
 /**
  * Handle sending out and receiving a response to validate the captcha
@@ -8,26 +6,26 @@ namespace Greggilbert\Recaptcha;
 class CheckRecaptchaV2 implements RecaptchaInterface
 {
 
-	/**
-	 * Call out to reCAPTCHA and process the response
-	 * @param string $challenge
-	 * @param string $response
-	 * @return bool
-	 */
-	public function check($challenge, $response)
-	{
-		$parameters = http_build_query(array(
-			'secret'        => app('config')->get('recaptcha::config.private_key'),
-			'remoteip'		=> app('request')->getClientIp(),
-			'response'      => $response,
-		));
-        
+    /**
+     * Call out to reCAPTCHA and process the response
+     *
+     * @param string $challenge
+     * @param string $response
+     * @return bool
+     */
+    public function check($challenge, $response)
+    {
+        $parameters = http_build_query(array(
+            'secret'   => app('config')->get('recaptcha::config.private_key'),
+            'remoteip' => app('request')->getClientIp(),
+            'response' => $response,
+        ));
+
         $url = 'https://www.google.com/recaptcha/api/siteverify?' . $parameters;
         $checkResponse = null;
-        
+
         // prefer curl, but fall back to file_get_contents
-        if('curl' === app('config')->get('recaptcha::config.driver') && function_exists('curl_version'))
-        {
+        if ('curl' === app('config')->get('recaptcha::config.driver') && function_exists('curl_version')) {
             $curl = curl_init($url);
             curl_setopt($curl, CURLOPT_HEADER, false);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -36,25 +34,23 @@ class CheckRecaptchaV2 implements RecaptchaInterface
 
             $checkResponse = curl_exec($curl);
         }
-        else
-        {
+        else {
             $checkResponse = file_get_contents($url);
         }
-        
-        if(is_null($checkResponse) || empty($checkResponse))
-        {
+
+        if (is_null($checkResponse) || empty($checkResponse)) {
             return false;
         }
-        
+
         $decodedResponse = json_decode($checkResponse, true);
-		return $decodedResponse['success'];
-	}
+        return $decodedResponse['success'];
+    }
 
     public function getTemplate()
     {
         return 'captchav2';
     }
-    
+
     public function getResponseKey()
     {
         return 'g-recaptcha-response';
